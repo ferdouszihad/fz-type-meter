de1374175b5716d0b29f28142e07bcf940b83573
Your Name~ <f.zihad333@gmail.com>
2022-09-15 01:09:31 +0600

    Second is now not showing its decimal value

--- script.js
+++ script.js
@@ -84,8 +84,9 @@ const gameOver = () => {
   // show result
   resultModal.innerHTML += `
     <h1>Finished!</h1>
-    <p>You took: <span class="bold">${timeTaken}</span> seconds</p>
-    <p>You made <span class="bold red">${errorCount}</span> mistakes</p>
+    <p class="gap-down-10">You took: 
+    <span class="bold">${timeTaken.toFixed(2)}</span> seconds</p>
+    <p class="gap-down-10">You made <span class="bold red">${errorCount}</span> mistakes</p>
     <button onclick="closeModal()">Close</button>
   `;
 
@@ -139,6 +140,6 @@ setInterval(() => {
   const timeSpent = (currentTime - startTime) / 1000;
 
   document.getElementById("show-time").innerHTML = `${
-    startTime ? timeSpent : 0
+    startTime ? timeSpent.toFixed(0) : 0
   } seconds`;
 }, 1000);
